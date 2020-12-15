9d63bf98a1f971c1aec5777441548d72527d2cba
Carmen <kohl.carmen.1@gmail.com>
2020-12-15 12:26:11 -0500

    tidy vbm

--- Correlations_VBM.m
+++ Correlations_VBM.m
@@ -1,20 +1,29 @@
-%% Plot VBM Correlations
-% Creates figure 4 in manuscript:
-% correlations between MT/MIP grey matter and MT/MIP effect
-% MIP/MT GM were extracted from scans (see VBM Notes Google Docs). MIP was
-% based on where we found a signifcant effect. MT was based on TMS
-% coordinates.
-% The TMS effect is taken from the behavioural analysis. The Predictor
-% D-HV, but non-tms subtracted from tms. So MIP_1_contra - MIP_0_contra
-% (same for MT).
+%% VBM Correlations
+% - runs correlations between  MT/MIP grey matter and MT/MIP effect
+% - plots Figure 4 b, c
+
+
+%   MIP/MT GM were extracted from scans (see VBM Notes Google Docs). MIP was
+%   based on where we found a signifcant effect. MT was based on TMS
+%   coordinates.
+
+%   The TMS effect is taken from the behavioural analysis. The Predictor
+%   D-HV, but non-tms subtracted from tms. So MIP_1_contra - MIP_0_contra
+%   (same for MT). You can recreate what's loaded here as a design (e.g.
+%   D_MIP) by running GLM3_Beh.m and subtracted contralateral non-tms from 
+%   tms (e.g. MIP1contra-MIP0contra).
 
 clear
-corr_or_partialcorr=1;
+corr_or_partialcorr=2;
 
-%load MIP GM
-filename = 'D:\PolyU\TMS\Results\VBM\output\GM_MIP.txt';
+file_dir = 'D:\PolyU\TMS\Results\VBM\output\';
 plot_dir='D:\PolyU\TMS\Paper\';
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% load
+
+%load MIP GM
+filename = strcat(file_dir,'GM_MIP.txt');
 delimiter = ' ';
 formatSpec = '%s%[^\n\r]';
 fileID = fopen(filename,'r');
@@ -28,7 +37,7 @@ end
 MIP=MIP';
 
 %load MT GM
-filename = 'D:\PolyU\TMS\Results\VBM\output\GM_MT.txt';
+filename = strcat(file_dir,'GM_MT.txt');
 delimiter = ' ';
 formatSpec = '%s%[^\n\r]';
 fileID = fopen(filename,'r');
@@ -42,7 +51,7 @@ end
 MT=MT';
 
 %load MIP design
-filename = 'D:\PolyU\TMS\Results\VBM\output\MIP_Test_Design.txt';
+filename = strcat(file_dir,'MIP_Test_Design.txt');
 delimiter = '\t';
 formatSpec = '%*s%f%f%f%[^\n\r]';
 fileID = fopen(filename,'r');
@@ -53,7 +62,7 @@ MIPtemp2 = dataArray{:, 2};
 D_MIP = dataArray{:, 3};
 
 %load MT design
-filename = 'D:\PolyU\TMS\Results\VBM\output\MT_Test_Design.txt';
+filename = strcat(file_dir,'MT_Test_Design.txt');
 delimiter = '\t';
 formatSpec = '%*s%f%f%f%[^\n\r]';
 fileID = fopen(filename,'r');
@@ -63,17 +72,19 @@ MTtemp1 = dataArray{:, 1};
 MTtemp2 = dataArray{:, 2};
 D_MT = dataArray{:, 3};
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% correlate
 if corr_or_partialcorr==1
     %correlations
     [r p]=corr(MIP,D_MIP);
-    % [r p] = corr(MIP,D_MIP, 'type', 'Spearman')
-    MIP_MIP=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p);
+    MIP_MIP=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p)
     [r p]=corr(MIP,D_MT);
-    MIP_MT=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p);
+    MIP_MT=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p)
     [r p]=corr(MT,D_MIP);
-    MT_MIP=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p);
+    MT_MIP=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p)
     [r p]=corr(MT,D_MT);
-    MT_MT=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p);
+    MT_MT=sprintf('r(%i) = %2.2f, p = %2.3f',length(MIP)-2,r,p)
 elseif corr_or_partialcorr==2
     %partial correlations
     [r p]=partialcorr(MIP,D_MIP,[MIPtemp1,MIPtemp2]);
@@ -88,12 +99,12 @@ end
 
 %exclude outlier
 outlier=find(D_MIP==max(D_MIP));
-fprintf('Exclude outlier: \n')
+fprintf('Exclude outlier (partial correlation): \n')
 [r_check p_check]=partialcorr(MIP([1:outlier-1,outlier+1:end]),D_MIP([1:outlier-1,outlier+1:end]),[MIPtemp1([1:outlier-1,outlier+1:end]),MIPtemp2([1:outlier-1,outlier+1:end])]);
 fprintf('r(%i) = %2.2f, p = %2.3f \n',length(MIP([1:outlier-1,outlier+1:end]))-4,r_check,p_check)
-
-%plot regressions
-clf
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% plot
+figure
 %1: MIP, MIP_Test
 subplot (2,2,1)
 P=polyfit(D_MIP,MIP,1);
@@ -106,6 +117,7 @@ set(gca,'YTick', [.1 .3 .5 .7 .9])
 ylim([.1 .8])
 set(gca,'XTick', [-2 -1 0 1 2])
 title(MIP_MIP)
+
 %2: MIP, MT_Test
 subplot (2,2,2)
 P=polyfit(D_MT,MIP,1);
@@ -118,6 +130,7 @@ set(gca,'YTick', [.1 .3 .5 .7 .9])
 ylim([.1 .8])
 set(gca,'XTick', [-2 -1 0 1 2])
 title(MIP_MT)
+
 %3: MT, MIP_Test
 subplot (2,2,3)
 P=polyfit(D_MIP,MT,1);
@@ -130,6 +143,7 @@ set(gca,'YTick', [.1 .3 .5 .7 .9])
 ylim([.1 .7])
 set(gca,'XTick', [-2 -1 0 1 2])
 title(MT_MIP)
+
 %4: MT, MT_Test
 subplot (2,2,4)
 P=polyfit(D_MT,MT,1);
